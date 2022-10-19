672a3d90c2496188cac4bfa3301552e6e5becc23
ImbarInformatika <imbarps16@gmail.com>
2022-10-19 08:04:34 -0700

    hari ke-3

---
+++ tipedataboolean.java
@@ -0,0 +1,7 @@
+package rpl;
+public class tipedataboolean {
+    public static void main(String[] args) {
+        boolean mahasiswa = true;
+        System.out.println("Saldo saya sekarang : "+ mahasiswa);
+    }
+}
