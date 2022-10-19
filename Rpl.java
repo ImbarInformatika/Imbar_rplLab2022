567e182618352418d1ed3edc1b20ec87ec3b6680
ImbarInformatika <imbarps16@gmail.com>
2022-10-19 07:30:21 -0700

    hari ke-2

---
+++ Rpl.java
@@ -0,0 +1,8 @@
+package rpl;
+public class Rpl {
+    public static void main(String[] args) {
+        char ukuran_baju = 'M';
+        System.out.println("Ukuran Baju Saya Adalah : "+ ukuran_baju);
+    }
+    
+}
