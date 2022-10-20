e438f6577f7c3a514ee3251caaec595aa11b919e
ImbarInformatika <imbarps16@gmail.com>
2022-10-20 04:02:45 -0700

    hari ke-4

---
+++ variabel.java
@@ -0,0 +1,9 @@
+package rpl;
+public class variabel {
+    public static void main(String[] args) {
+        String nama = "imbar";
+        System.out.println("Nama saya : " + nama);
+        //untuk membuat variabel kita tinggal mengetikkan "tipe_data namavariabel = "isi variabel"
+    }
+    
+}
