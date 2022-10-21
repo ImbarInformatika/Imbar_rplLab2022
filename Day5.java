0b0b7b4edd6566981c73d267eb2071de01933bbe
ImbarInformatika <imbarps16@gmail.com>
2022-10-21 04:51:26 -0700

    hari ke-5

---
+++ Day5.java
@@ -0,0 +1,17 @@
+package rpl;
+public class Day5 {
+    public static void main(String[] args) {
+        int umur, nomor_sepatu;
+        String nama, alamat;
+        
+        nama = "Imbar";
+        alamat = "Malunda";
+        umur = 19;
+        nomor_sepatu = 45;
+        
+        System.out.println("Nama    : " + nama);
+        System.out.println("Alamat  : " + alamat);
+        System.out.println("Umur    : " + 19);
+        System.out.println("Nomor Sepatu : " + nomor_sepatu);
+    }
+}
