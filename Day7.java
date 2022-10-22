627ae8a71576dff44f8b9252484c408f73064bd8
ImbarInformatika <imbarps16@gmail.com>
2022-10-22 16:37:50 -0700

    hari ke-7

---
+++ Day7.java
@@ -0,0 +1,17 @@
+package rpl;
+
+import java.util.Scanner;
+
+public class Day7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("masukkan nama Anda : ");
+        String nama = input.nextLine();
+        
+        System.out.println();
+        System.out.print("Masukkan tahun lahir Anda : ");
+        int tahun_lahir = input.nextInt();
+        int usia = 2022 - tahun_lahir;
+        System.out.println("Umur kamu : " + usia);
+    }
+}
