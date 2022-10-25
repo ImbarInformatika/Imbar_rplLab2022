6c0b51758b23df45ea66f45f8de90b8c9eec4878
ImbarInformatika <imbarps16@gmail.com>
2022-10-25 00:29:53 -0700

    hari ke-9

---
+++ Day9.java
@@ -0,0 +1,23 @@
+package rpl;
+
+import java.util.Scanner;
+
+public class Day9 {
+    public static void main(String[] args) {
+        int gaji = 1000000;
+        int gaji_lembur = 100000;
+        int pajak = 5/100; //10 persen
+        Scanner input = new Scanner (System.in);
+        System.out.print("Hari lembur : ");
+        int hari_lembur = input.nextInt();
+        
+        int gaji_bersih = gaji + (gaji_lembur * hari_lembur);
+        System.out.println("Gaji bersih : "+ gaji_bersih);
+        
+        int total_pajak = gaji * 5/100;
+        System.out.println("pajak total : "+ total_pajak);
+        
+        int gaji_total = gaji_bersih - total_pajak;
+        System.out.println("gaji total : "+ gaji_total);
+    }
+}
