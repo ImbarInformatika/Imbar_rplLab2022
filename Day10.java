f4405d2151c8384571f12942cd4a2deeffa0e004
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-10-26 08:29:01 -0700

    "hari ke-10"
    "hari ke-10"

---
+++ Day10.java
@@ -0,0 +1,14 @@
+package rpl;
+public class Day10 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        
+        System.out.println(a + " SAMA DENGAN "+ b + " = " + (a == b));
+        System.out.println(a + " TIDAK SAMA DENGAN "+ b + " = " + (a != b));
+        System.out.println(a + " LEBIH BESAR ATAU SAMA DENGAN "+ b + " = " + (a >= b));
+        System.out.println(a + " LEBIH KECIL ATAU SAMA DENGAN "+ b + " = " + (a <= b));
+        System.out.println(a + " LEBIH BESAR DARI "+ b + " = " + (a > b));
+        System.out.println(a + " LEBIH KECIL DARI "+ b + " = " + (a < b));
+    }
+} 
