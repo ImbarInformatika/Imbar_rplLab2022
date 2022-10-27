e728c3d4395c9a35f9ee48acb01a156ae8c401e5
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-10-27 08:10:52 -0700

    hari ke-11
    hari ke-11

---
+++ Day11.java
@@ -0,0 +1,23 @@
+package rpl;
+
+import java.util.Scanner;
+
+public class Day11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Masukkan nama Kamu : ");
+        String nama = input.nextLine();
+        
+        switch (nama)
+            {
+                case "imbar" :
+                    System.out.println(nama+ " merupakan mahasiswa informatika semester tiga");
+                case "rasul" :
+                    System.out.println(nama+ " merupakan mahasiswa informatika semester tiga");
+                default:
+                    System.out.println(nama+ " bukan merupakan mahasiswa informatika semester tiga");
+                    
+        }
+    }
+    
+}
