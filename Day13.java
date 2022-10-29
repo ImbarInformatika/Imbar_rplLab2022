99deb0067697a9eb7fd59d9593ae763482eccc0c
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-10-29 05:54:15 -0700

    hari ke-13
    hari ke-13

---
+++ Day13.java
@@ -0,0 +1,10 @@
+package rpl;
+public class Day13 {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i < 5)
+            System.out.println("Ini bilangan ke : " + i);
+            i += 1;
+    }
+}
