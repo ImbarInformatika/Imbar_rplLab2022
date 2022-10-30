92c714afe7e9b94c1dd2b0afd3d6f32a9b2d15b6
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-10-30 06:48:32 -0700

    hari ke-14
    hari ke-14

---
+++ Day14.java
@@ -0,0 +1,20 @@
+package rpl;
+public class Day14 {
+    public static void main(String[] args) {
+        int a = 6;
+        System.out.print("Hasil Penjumlahan += : ");
+        System.out.println(a += 3);
+        
+        System.out.print("Hasil Pengurangan -= : ");
+        System.out.println(a -= 2);
+        
+        System.out.print("Hasil Perkalian *= : ");
+        System.out.println(a *= 2);
+        
+        System.out.print("Hasil Pembagian /= : ");
+        System.out.println(a /= 4);
+        
+        System.out.print("Hasil Sisa Pembagian += : ");
+        System.out.println(a %= 2);
+    }
+}
