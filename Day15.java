d9a4732f0726a81c5c06676da101d4a590a9cc33
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-10-31 20:57:11 +0800

    hari ke-15
    hari ke-15

---
+++ Day15.java
@@ -0,0 +1,12 @@
+package day15;
+public class Day15 {
+    public static void main(String[] args) {
+       for(int angka=0; angka<7; angka++){
+            if(angka == 5){
+                break; //jika nilainya sama dengan 5 maka program akan berhenti walaupun batasnya 7
+            }else{
+            System.out.println(angka);
+            }
+        }
+    }
+}
