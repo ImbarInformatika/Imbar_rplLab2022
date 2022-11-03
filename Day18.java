88a3dd8fb21019642282b8fe934fdf2e395d2e91
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-03 10:49:59 +0800

    hari ke-18
    hari ke-18

---
+++ Day18.java
@@ -0,0 +1,11 @@
+package rpl;
+
+import java.util.Arrays;
+
+public class Day18 {
+    public static void main(String[] args) {
+        String teman[] = {"imbar", "amri", "sayyid", "ikbal", "sofyan"};
+        
+        System.out.println("Sodara : " + Arrays.toString(teman));
+    }
+}
