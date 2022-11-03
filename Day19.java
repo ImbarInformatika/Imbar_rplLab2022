2bceb2eefbc1aab08e95d30700473f58b9f5771b
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-03 10:57:37 +0800

    hari ke-19

---
+++ Day19.java
@@ -0,0 +1,10 @@
+package rpl;
+public class Day19 {
+    public static void main(String[] args) {
+        String teman[] = {"imbar", "amri", "sayyid", "ikbal", "sofyan"};
+        
+        for(String b : teman){
+            System.out.println("Sodara : " + b);
+        }
+    }
+}
