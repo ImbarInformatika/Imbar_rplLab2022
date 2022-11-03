efebb8a526cb2e03e4e97acd33bf2a31afb2d641
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-03 10:59:54 +0800

    hari ke-20

---
+++ Day20.java
@@ -0,0 +1,8 @@
+package rpl;
+public class Day20 {
+    public static void main(String[] args) {
+        String teman[] = {"imbar", "amri", "sayyid", "ikbal", "sofyan"};
+        
+        System.out.println("Panjang array teman adalah : "+(teman).length);
+    }
+}
