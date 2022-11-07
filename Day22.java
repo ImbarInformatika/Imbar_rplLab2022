26940eba7958dfb63fcaed0e03cf3f031a1d85ae
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-07 20:09:33 +0800

    Membuat ArrayList

---
+++ Day22.java
@@ -0,0 +1,14 @@
+package rpl;
+
+import java.util.ArrayList;
+
+public class Day22 {
+    public static void main(String[] args) {
+        ArrayList imbar = new ArrayList();
+        
+        imbar.add("malunda");
+        imbar.add(16);
+        imbar.add(true);
+        System.out.println("imbar : "+ imbar);
+        }
+}
