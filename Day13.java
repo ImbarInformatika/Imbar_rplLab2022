f30d6efb5561c6541b689d1137f770d7f0ac00a8
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-08 01:33:10 -0800

    Perulangan While

--- Day13.java
+++ Day13.java
@@ -3,7 +3,7 @@ public class Day13 {
     public static void main(String[] args) {
         int i = 0;
         
-        while (i < 5)
+        while (i < 6)
             System.out.println("Ini bilangan ke : " + i);
             i += 1;
     }
