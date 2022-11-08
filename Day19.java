cc0e5bc3f7e6d4d8f05f28d490d8c91cf116436a
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-08 01:44:35 -0800

    Cara lain Menampilkan Array

--- Day19.java
+++ Day19.java
@@ -1,7 +1,7 @@
 package rpl;
 public class Day19 {
     public static void main(String[] args) {
-        String teman[] = {"imbar", "amri", "sayyid", "ikbal", "sofyan"};
+        String teman[] = {"Imbar", "amri", "sayyid", "ikbal", "sofyan"};
         
         for(String b : teman){
             System.out.println("Sodara : " + b);
