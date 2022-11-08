26c8ca5e0c13dde235d3a5af5b54f2143ec82cbe
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-08 01:43:40 -0800

    Menampilkan Isi Array

--- Day18.java
+++ Day18.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Day18 {
     public static void main(String[] args) {
-        String teman[] = {"imbar", "amri", "sayyid", "ikbal", "sofyan"};
+        String teman[] = {"Imbar", "amri", "sayyid", "ikbal", "sofyan"};
         
         System.out.println("Sodara : " + Arrays.toString(teman));
     }
