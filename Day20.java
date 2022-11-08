59883ccae29a0368145f84289539fc5dba81f567
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-08 01:45:22 -0800

    Menampilkan Panjang Array

--- Day20.java
+++ Day20.java
@@ -1,7 +1,7 @@
 package rpl;
 public class Day20 {
     public static void main(String[] args) {
-        String teman[] = {"imbar", "amri", "sayyid", "ikbal", "sofyan"};
+        String teman[] = {"Imbar", "amri", "sayyid", "ikbal", "sofyan"};
         
         System.out.println("Panjang array teman adalah : "+(teman).length);
     }
