9f1df170372570a85859aa56984c8301d3b221d7
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-11 23:31:45 +0800

    Operator Logika NOT

---
+++ Day26.java
@@ -0,0 +1,10 @@
+package rpl;
+public class Day26 {
+    public static void main(String[] args) {
+        boolean a = true ;
+        boolean b = false;
+        
+        boolean hasil = !a; //jika a bernilai true maka hasilnya akan false begitupun sebaliknya
+        System.out.println("hasil : " + hasil);
+    }
+}
