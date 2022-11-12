02eb12be238c9891a8624632571d80ae3ddb81b8
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-12 22:28:21 +0800

    Opertaor Logika OR

---
+++ Day27.java
@@ -0,0 +1,10 @@
+package rpl;
+public class Day27 {
+    public static void main(String[] args) {
+        boolean a = true ;
+        boolean b = false;
+        
+        boolean hasil = a || b; //jika salah satunya bernilai true maka hasilnya akan true
+        System.out.println("hasil : " + hasil);
+    } 
+}
