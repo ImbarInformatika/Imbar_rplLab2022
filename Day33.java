23dc4acae87a0bd69ebb4b9eba9e3219eeee9638
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-18 15:42:41 +0800

    Add files via upload
    Deret Bilangan

---
+++ Day33.java
@@ -0,0 +1,13 @@
+package rpl;
+public class Day33 {
+    public static void main(String[] args) {
+        int deret_nilai, i;
+        deret_nilai = 5;
+        
+        for (i = 0; i <= deret_nilai; i++){
+            System.out.println(i + " ");
+        }
+        System.out.println("");
+    }
+    
+}
