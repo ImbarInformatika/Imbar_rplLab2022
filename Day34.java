919439d87d1e07375e77b5a64e36ee297350639b
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-19 08:06:37 +0800

    Add files via upload
    Perulangan For

---
+++ Day34.java
@@ -0,0 +1,12 @@
+package rpl;
+public class Day34 {
+    public static void main(String[] args) {
+       
+        for(int batas = 1; batas < 8; batas++){
+            System.out.println("Hitungan ke-" + batas);
+            //bilangan i akan di tampilkan dari 1
+            //bilangan 1 akan terus di tambah 1 sampai i = 7
+            //mengapa 7 karna batas nya 8 dan pembatas tidak dihitung
+        }
+    }
+}
