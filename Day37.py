c6831aa3c02d6d4c2b9245da59815d77c7d07756
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-22 19:40:33 +0800

    Add files via upload
    Cara Membuat Fungsi

---
+++ Day37.py
@@ -0,0 +1,5 @@
+def imbar(): #def adalah kata kunci sedangkan imbar adalah fungsi
+    print("Nama : Imbar")#isi dari fungsi
+    print("Alamat : Malunda")#isi dari fungsi
+
+imbar()#pemanggilan fungsi
