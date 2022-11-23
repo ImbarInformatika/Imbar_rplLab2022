3c4448e939abf8c972541e00856e0d5873134af8
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-23 22:09:29 +0800

    Add files via upload
    Menampilkan Fungsi Dengan Inputan

---
+++ 38.py
@@ -0,0 +1,11 @@
+def identitas():
+    print("nama = imbar")
+    print("alamat = malunda")
+    print("umur = 20")
+
+tampilkan = input("masukkan yang ingin di tampilkan : ")
+if tampilkan == "1":
+    identitas()
+
+else:
+    print("tidak ada")
