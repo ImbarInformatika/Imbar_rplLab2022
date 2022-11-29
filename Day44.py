5d309fd0813bafaee051b974f79b13cd3f869ec1
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-29 03:51:33 -0800

    Mengurutkan Inputan

---
+++ Day44.py
@@ -0,0 +1,10 @@
+satu = int(input("MASUKKAN ANGKA : "))
+dua = int(input("MASUKKAN ANGKA : "))
+tiga = int(input("MASUKKAN ANGKA : "))
+empat = int(input("MASUKKAN ANGKA : "))
+lima = int(input("MASUKKAN ANGKA : "))
+
+doko = [satu, dua, tiga, empat, lima]
+print("sebelum di urutkan : ", doko)
+print("setelah di urutkan : ", sorted(doko))
+ 
