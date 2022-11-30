3dad4d779775c51698354d3f047c94e862180cf0
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-11-30 07:05:00 -0800

    Konversi String ke Integer

---
+++ Day45.py
@@ -0,0 +1,13 @@
+sebelum_dikonversi = input("MASUKKAN : ")
+print("DATA : ", sebelum_dikonversi)
+print("TIPE DATA : ", type(sebelum_dikonversi))
+
+print()
+dikonversi = float(sebelum_dikonversi)
+print("DATA : ", dikonversi)
+print("TIPE DATA : ", type(dikonversi))
+
+print()
+setelah_dikonversi = int(sebelum_dikonversi)
+print("DATA : ", setelah_dikonversi)
+print("TIPE DATA : ", type(setelah_dikonversi))
