7f018b2e3adde5ec39a2d83b8c5207768d88aa07
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-12-01 22:15:47 +0800

    Kapital Kecil

---
+++ 46.py
@@ -0,0 +1,11 @@
+#Menggunakan lower()
+angka = "MENGUBAH HURUF KAPITAL MENJADI HURUF KECIL"
+print("sebelum menggunakan lower : ", angka)
+print("setelah menggunakan lower : ", angka.lower())
+
+
+#menggunakan lower()
+print()
+angka = "mengubah huruf kecil menjadi huruf besar"
+print("sebelum menggunakan upper : ", angka)
+print("setelah menggunakan upper : ", angka.upper())
