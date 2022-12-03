707b9b27a68ae18c678251e90d7b8f278207df72
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-12-03 22:33:13 +0800

    Int ke String

---
+++ Day48.py
@@ -0,0 +1,8 @@
+sebelum_dikonversi = int(input("MASUKKAN : "))
+print("DATA : ", sebelum_dikonversi)
+print("TIPE DATA : ", type(sebelum_dikonversi))
+
+print()
+setelah_dikonversi = str(sebelum_dikonversi)
+print("DATA : ", setelah_dikonversi)
+print("TIPE DATA : ", type(setelah_dikonversi))
