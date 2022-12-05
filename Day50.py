2b29ebd775feaeab42f52ee458b8fb5019423186
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-12-05 23:28:23 +0800

    menampilkan List

---
+++ Day50.py
@@ -0,0 +1,10 @@
+list = ["imbar", "imran", "edwin", "amri", "ikbal"]
+
+print(list[0]) # memanggil list berdasarkan index
+print(list[0:3]) #memanggil list index 0 sampai index 2
+print(list[-1])#memanggil list dengan index terakhir
+print(list) #menampilkan semua isi list
+
+print()
+for i in list: #Menampilkan list dengan perulangan for
+    print(i)
