87f258df12a937747c5770c9cf4379127e12b10a
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2022-12-10 21:06:31 +0800

    Membalik List

---
+++ Day55.py
@@ -0,0 +1,11 @@
+satu = int(input("MASUKKAN ANGKA : "))
+dua = int(input("MASUKKAN ANGKA : "))
+tiga = int(input("MASUKKAN ANGKA : "))
+empat = int(input("MASUKKAN ANGKA : "))
+lima = int(input("MASUKKAN ANGKA : "))
+
+doko = [satu, dua, tiga, empat, lima]
+print("sebelum di balik : ", doko)
+doko.reverse()
+print("setelah di balik : ", doko)
+ 
