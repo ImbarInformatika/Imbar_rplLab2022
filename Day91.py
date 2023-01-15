81c3e4500d4f3db552cc21e75eb7b27f629379dc
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2023-01-16 07:19:25 +0800

    Add files via upload

---
+++ Day91.py
@@ -0,0 +1,9 @@
+#Pangkat Bilangan
+
+bilangan = int(input("Masukkan bilangan : "))
+print()
+pangkat = int(input("Masukkan bilangan pangkat : "))
+
+print()
+hasil = bilangan ** pangkat
+print(f"Hasil dari {bilangan} pangkat {pangkat} adalah : ", hasil)
