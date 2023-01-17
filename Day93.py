4ef1d6b5929093fb3e38e7d52f72f226d7bc1fc7
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2023-01-17 09:38:15 +0800

    Add files via upload

---
+++ Day93.py
@@ -0,0 +1,9 @@
+#Perpangkatan dengan fungsi pow
+
+bilangan = int(input("Masukkan bilangan : "))
+print()
+pangkat = int(input("Masukkan pangkat : "))
+print()
+
+hasil = pow(bilangan, pangkat)
+print(f"Hasil dari {bilangan} pangkat {pangkat} adalah : ", hasil)
