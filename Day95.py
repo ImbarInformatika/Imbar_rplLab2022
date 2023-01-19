e0ee35f1ee7288e0166ebe4e1c7bb87ab78f9a06
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2023-01-19 20:18:14 +0800

    Add files via upload

---
+++ Day95.py
@@ -0,0 +1,13 @@
+#Menentukan angka positif dan negatif
+angka = int(input("Tulis Sebuah Angka: "))
+print()
+if angka > 0:
+   print("Positif ", angka, "atau biasa di tulis", angka)
+
+#Menampilkan Kondisi Angka Nol   
+elif angka == 0:
+   print("Angka", angka, "atau biasa di tulis", angka)
+
+#Menampilkan Kondisi Angka Negatif
+else:
+   print("Negatif", angka, "atau biasa di tulis", angka)
