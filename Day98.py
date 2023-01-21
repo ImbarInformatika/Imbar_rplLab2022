7f11d586cb28f78d95604301fa3b3477682c1480
ImbarInformatika <100125416+ImbarInformatika@users.noreply.github.com>
2023-01-21 12:31:25 +0800

    Add files via upload

---
+++ Day98.py
@@ -0,0 +1,10 @@
+#Menghitung gaji perhari buru
+
+jumlah_uang = float(input("Masukkan Jumlah Uang : "))
+print()
+
+jumlah_harian = float(input("Masukkan keseluruhan Harian : "))
+print()
+
+jumlah = jumlah_uang / jumlah_harian
+print("Total gaji kotor harian buru adalah : ", jumlah)
